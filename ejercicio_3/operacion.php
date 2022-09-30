9a7d741a75face81f62055973c969d9d1a69b79b
arturordx <56272033+arturordx@users.noreply.github.com>
2022-09-30 10:41:43 -0300

    Add files via upload

---
+++ ejercicio_3/operacion.php
@@ -0,0 +1,20 @@
+<?php
+
+$a=$_POST['a'];
+$b=$_POST['b'];
+$c=$_POST['c'];
+
+$raiz = sqrt(($b**2) - (4 * $a * $c));
+
+if (($b**2) - (4 * $a * $c)>=0){
+
+$x1 = ($b * -1) +  $raiz / (2 * $a);
+$x2 = ($b * -1) - $raiz / (2 * $a);
+
+echo "X1 es: $x1"; 
+echo "X2 es: $x2";
+
+}else{
+echo "no se puede calcular por tener una raiz negativa";
+}
+?>
